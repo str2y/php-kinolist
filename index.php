8cf88ceb6483c51e48cdbd6c6723af16d96858e9
aldrei <aldreicr@gmail.com>
2025-01-30 15:31:58 -0300

    added navbar

--- index.php
+++ index.php
@@ -15,26 +15,106 @@
             <div class="flex">
                 <img class="h-20" src="assets/Vector/Logo.svg" alt="Logo">
             </div>
-            <ul class="ml-28 rounded-lg flex text-2xl gap-20">
-                <li class="flex items-center gap-3 px-4 py-2 rounded-lg transition ease-in-out delay-50 hover:bg-gray-800 active:bg-gray-700 active:text-purple-500 duration-200"><i class="ph ph-popcorn"></i><a href="/">Explore</a></li>
-                <li><a href="/my-kinos" class="px-4 py-2 rounded-lg transition ease-in-out delay-50 hover:bg-gray-800 active:bg-gray-700 active:text-purple-500 duration-200">My Kinos</a></li>
-            </ul>
-            <ul class="text-lg text-gray-300 flex gap-6">
-                <li>Hello, user</li>
-                <li>Logout</li>
-            </ul>
+            <menu class="ml-28 rounded-lg flex text-2xl gap-20">
+                <a class="rounded-lg" href="/">
+                    <button class="flex items-center gap-3 px-4 py-2 rounded-lg transition ease-in-out delay-50 hover:bg-gray-800 focus-within:bg-gray-700 focus-within:text-purple-500 duration-200">
+                        <i class="ph ph-popcorn"></i>
+                        Explore
+                    </button>
+                </a>
+                <a class="rounded-lg" href="/my-kinos">
+                    <button class="flex items-center gap-3 px-4 py-2 rounded-lg transition ease-in-out delay-50 hover:bg-gray-800 focus-within:bg-gray-700 focus-within:text-purple-500 duration-200">
+                        <i class="ph ph-film-slate"></i>
+                        My Kinos
+                    </button>
+                </a>
+            </menu>
+            <menu class="items-center text-lg text-gray-300 flex gap-4">
+                <div class="flex items-center gap-2">
+                    <a href="/login" class="hover:underline">Login</a>
+                    <button class="flex items-center bg-gray-800 rounded-lg px-2.5 py-2 hover:bg-gray-700 text-gray-500 hover:text-purple-500">
+                        <i class="text-4xl ph ph-user"></i>
+                    </button>
+                </div>
+                <div class="min-h-12 border border-gray-700"></div>
+                <a class="rounded-lg" href="/">
+                    <button class="flex items-center bg-gray-800 rounded-lg px-2.5 py-2 hover:bg-gray-700 text-gray-500 hover:text-purple-500">
+                        <i class="text-4xl ph ph-sign-out"></i>
+                    </button>
+                </a>
+            </menu>
         </nav>
     </header>
     <main class="mx-auto max-w-screen-2xl pb-10">
         <div class="flex justify-between pt-20">
-            <div class="text-3xl font-bold text-gray-300">Explore</div>
-            <form class="flex space-x-2 text-gray-500">
-                <input type="text" name="pesquisar" class="border-gray-700 border rounded bg-gray-900 text-lg focus:outline-none px-4 py-2" placeholder="Search kino">
-                <button type="submit">🔍</button>
-            </form>
+            <div class="text-4xl font-bold text-gray-300">Explore</div>
+            <div class="flex items-center px-2 py-2 text-gray-500 border rounded border-gray-700 focus-within:text-purple-500 focus-within:border-purple-500">
+                <i class="text-2xl ph ph-magnifying-glass"></i>
+                <input type="text" name="search" class="px-2 bg-gray-900 text-lg placeholder-gray-500 text-gray-300 focus:outline-none" placeholder="Search kino">
+            </div>
         </div>
         <!-- movies list -->
-        <section class="flex grid grid-cols-1 md:grid-cols-2 lg:grid-cols gap-4">
+        <section class="h-screen mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div class="min-h-1/2 p-2 rounded-2xl border-stone-800 border-2 bg-stone-900">
+                <div class="flex gap-2">
+                    <div class="w-1/3">
+                        <img src="" class="w-60 rounded">
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <a href="/" class="font-semibold hover:underline">nome</a>
+                        <div class="text-xs italic">genero</div>
+                        <div class="text-xs italic">nota</div>
+                    </div>
+                </div>
+            </div>
+            <div class="min-h-1/2 p-2 rounded-2xl border-stone-800 border-2 bg-stone-900">
+                <div class="flex gap-2">
+                    <div class="w-1/3">
+                        <img src="" class="w-60 rounded">
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <a href="/" class="font-semibold hover:underline">nome</a>
+                        <div class="text-xs italic">genero</div>
+                        <div class="text-xs italic">nota</div>
+                    </div>
+                </div>
+            </div>
+            <div class="min-h-1/2 p-2 rounded-2xl border-stone-800 border-2 bg-stone-900">
+                <div class="flex gap-2">
+                    <div class="w-1/3">
+                        <img src="" class="w-60 rounded">
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <a href="/" class="font-semibold hover:underline">nome</a>
+                        <div class="text-xs italic">genero</div>
+                        <div class="text-xs italic">nota</div>
+                    </div>
+                </div>
+            </div>
+            <div class="min-h-1/2 p-2 rounded-2xl border-stone-800 border-2 bg-stone-900">
+                <div class="flex gap-2">
+                    <div class="w-1/3">
+                        <img src="" class="w-60 rounded">
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <a href="/" class="font-semibold hover:underline">nome</a>
+                        <div class="text-xs italic">genero</div>
+                        <div class="text-xs italic">nota</div>
+                    </div>
+                </div>
+            </div>
+            <div class="min-h-1/2 p-2 rounded-2xl border-stone-800 border-2 bg-stone-900">
+                <div class="flex gap-2">
+                    <div class="w-1/3">
+                        <img src="" class="w-60 rounded">
+                    </div>
+                    <div class="flex flex-col gap-1">
+                        <a href="/" class="font-semibold hover:underline">nome</a>
+                        <div class="text-xs italic">genero</div>
+                        <div class="text-xs italic">nota</div>
+                    </div>
+                </div>
+            </div>
         </section>
     </main>
 </body>
---
+++ views/login.view.php
@@ -0,0 +1,5 @@
+<div class="bg-gray-900">
+    <div class="bg-blue-900 w-1/2">
+
+    </div>
+</div>
